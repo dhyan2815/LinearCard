e634e50ab388a07784599e7056fad2238cac1391
dhyan2815 <dhyan.work.2815@gmail.com>
2026-09-15 11:27:52 +0530

    chore: remove temporary migration and test scripts

--- apps/api/fix4.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const file = path.join(__dirname, 'src/passes/passes.controller.ts');
-let body = fs.readFileSync(file, 'utf-8');
-body = body.replace(/memberId: memberId: pass\.memberId as string,/g, 'memberId: pass.memberId as string,');
-fs.writeFileSync(file, body);
